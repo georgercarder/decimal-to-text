ab9dd48c992aa36b1d32bb5b8c5adacacc683e32
George Carder <georgercarder@gmail.com>
2017-09-20 19:37:17 -0700

    Update decimalwords.R

--- decimalwords.R
+++ decimalwords.R
@@ -1,12 +1,11 @@
 #!/bin/Rscript
 
 
-##TESTING: A script converting numeric representation of DIGITS to a text representation. 
+## A script converting numeric representation of DIGITS to a text representation. 
 
 #given n as a character
 
 # parse n=a.b to get a and b separately
-#find out if we reduce fractions. assuming not we continue
 a<-as.double(gsub("\\..*","",n))
 b<-as.double(gsub(".*\\.","",n))
 
